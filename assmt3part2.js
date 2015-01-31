9b0fcab61f7e85f9c80ea2c3a928470357b10aae
solomreb <solomreb@onid.oregonstate.edu>
2015-01-31 13:57:12 -0800

    modified .js

--- assmt3part2.js
+++ assmt3part2.js
@@ -1,9 +1,15 @@
 window.onload = function() {
-    document.getElementById('output').innerHTML = 'test';
+
 }
 
-
+function validateNumber(){
+    var x = document.forms["gistForm"]["number-input"].value;
+    if (x == null || x == "" || x<1 || x>5) {
+        alert("Number must be between 1 and 5");
+        return false;
+    }
+}
 function display(){
-    localStorage.setItem('number-Text',document.getElementsByName('number-input')[0].value);
-    document.getElementById('output').innerHTML = localStorage.getItem('number-Text');
+    localStorage.setItem('number',document.getElementsByName('number-input')[0].value);
+    document.getElementById('output').innerHTML = localStorage.getItem('number');
 }
