8bba48a2714a038e55b8e66f543c8728966f202f
solomreb <solomreb@onid.oregonstate.edu>
2015-01-31 15:19:36 -0800

    general form working

--- assmt3part2.js
+++ assmt3part2.js
@@ -1,6 +1,3 @@
-window.onload = function() {
-
-}
 
 function validateNumber(){
     var x = document.forms["gistForm"]["number-input"].value;
@@ -9,7 +6,40 @@ function validateNumber(){
         return false;
     }
 }
-function display(){
-    localStorage.setItem('number',document.getElementsByName('number-input')[0].value);
-    document.getElementById('output').innerHTML = localStorage.getItem('number');
-}
+
+     function getGists() {
+        var url = "https://api.github.com/search/repositories";
+        var xmlhttp = new XMLHttpRequest();
+        if (!xmlhttp) {
+            throw 'Unable to create HttpRequest.';
+        }
+        //var params = {
+        //    q: language 
+        //};
+        url += '?' + 'q=language:javascript';
+        xmlhttp.onreadystatechange = function() {
+            var text, content, html;
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                console.log("calling onreadystatechange function");
+                text = xmlhttp.responseText;
+                content = JSON.parse(text);
+                html = "<table>\n";
+                console.log(content.items.length);
+                for (var i = 0; i < content.items.length; i++) {
+                    html += "<tr><td>";
+                    html += i + ": " + content.items[i].description;
+                    html += "<td>";
+                    html += "<a href='" + content.items[i].url + "'>" + content.items[i].url + "</a>";
+                    html += "<tr>\n";
+                }
+                html += "</table>\n";
+                console.log(html);
+                document.getElementById("content").innerHTML = html;
+            }
+        }
+        console.log("opening URL " + url);
+        xmlhttp.open("GET",url,true);
+        xmlhttp.send();
+         
+         
+    }
