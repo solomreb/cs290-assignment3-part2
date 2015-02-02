23027ecb13b031b6c1852831f0c3b98c9b950b3b
solomreb <solomreb@onid.oregonstate.edu>
2015-02-01 22:01:40 -0800

    save favorites kind of working

--- assmt3part2.js
+++ assmt3part2.js
@@ -1,49 +1,86 @@
 
-function validateNumber(){
+function getPagesNumber(){
     var x = document.forms["gistForm"]["number-input"].value;
-    if (x == null || x == "" || x<1 || x>5) {
+    if (x === null || x === "" || x<1 || x>5) {
         alert("Number must be between 1 and 5");
         return false;
     }
+    return x;
 }
 
-     function getGists() {
-        var url = "https://api.github.com/search/";
-        var xmlhttp = new XMLHttpRequest();
-        if (!xmlhttp) {
-            throw 'Unable to create HttpRequest.';
-        }
-        url += "repositories?q=language:";
-        var x = document.querySelectorAll('input:checked');
-        for (i=0; i<x.length; i++)
-        {
+function getURL(pages){
+    var url = "https://api.github.com/search/";
+    url += "repositories?page=" + pages + "&q=language:";
+    var x = document.querySelectorAll('input:checked');
+    if (x>0 || x!=null){
+        for (i=0; i<x.length; i++){
             url += x[i].value;
+            if (i+1==x.length)
+                break;
             url += '+language:';
         }
-        console.log("Url= " + url);
+    }
+    return url;
+}
+
+function getGists() {
+    var pages = getPagesNumber();
+    for (var i=1; i <= pages; i++) {
+        var xmlhttp = new XMLHttpRequest();
+        var xmlhttp2 = new XMLHttpRequest();
+        if (!xmlhttp) {
+           throw 'Unable to create HttpRequest.';
+        }
         xmlhttp.onreadystatechange = function() {
             var text, content, html;
             if (xmlhttp.readyState==4 && xmlhttp.status==200) {
                 text = xmlhttp.responseText;
-                content = JSON.parse(text);
-                html = "<table>\n";
-                html += "<h2>Gists matching your search:</h2>";
-                html += "<tr><th>Description</th><th>Language</th></tr>";
-                for (var i = 0; i < content.items.length; i++) {
-                    html += "<tr><td>";
-                    html += i + ": ";
-                    html += "<a href='" + content.items[i].url + "'>" + content.items[i].description + "</a>";
-                    html += "<td>" + content.items[i].language;
-                    html += "<tr>\n";
-                }
-                html += "</table>\n";
-                console.log(html);
-                document.getElementById("content").innerHTML = html;
+                displayResults(JSON.parse(text));
             }
         }
+
+        var url = getURL(1);
         console.log("opening URL " + url);
         xmlhttp.open("GET",url,true);
         xmlhttp.send();
-         
-         
+
     }
+}
+        
+
+function displayResults(content) {
+    for ( i=1; i < content.items.length; i++ ) {
+        var item = document.createElement("li");
+        item.setAttribute("id", i);
+        document.getElementById("gistsList").appendChild(item);
+        
+        var link = document.createElement("a");
+        item.appendChild(link);
+        var linkText = document.createTextNode(content.items[i].description);
+        link.appendChild(linkText);
+        link.href = content.items[i].url; 
+        item.appendChild(link);
+        
+        var btn = document.createElement("button");
+        var btnTxt = document.createTextNode("Save as favorite");
+        btn.appendChild(btnTxt);
+        item.appendChild(btn);
+        
+        btn.onclick = function() {console.log("saving " + this.parentElement.id + " to favorites"); localStorage.setItem(this.parentElement.id, this.previousSibling); displayFavorites()};
+        
+        var br = document.createElement("br");
+        item.appendChild(br);   
+    } 
+
+}
+
+function displayFavorites() {
+    for ( i=1; i <= localStorage.length; i++ ) {
+        document.getElementById("favorites").innerHTML = localStorage.getItem(i);
+    }
+    
+}
+/*
+function deleteFavorite() {
+
+}*/
